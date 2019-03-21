1aeccc3e16e99c316855d20ad16677cfc91c236e
Kesha C Lake <keshalakemusic@gmail.com>
2019-03-21 01:53:30 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  my_new_array = []
+end
+
+def array_with_two_elements
+  things_that_should_be_free = ["love", "water"]
+end
+
+def first_element(array)
+  my_first_element = array[0]
+end
+
+def third_element(array)
+  my_third_element = array[2]
+end
+
+def last_element(array)
+  my_last_element = array.last
+end
+
+def first_element_with_array_methods(array)
+  first_country = array.first
+end
+
+def last_element_with_array_methods(array)
+  last_country = array.last
+end
+
+def length_of_array(array)
+  length = array.length
+end
